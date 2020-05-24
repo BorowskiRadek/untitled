dfd07383a40f3328b148f265b0fce5361f66b8e1
BorowskiRadek <45455509+radzik100@users.noreply.github.com>
2020-05-24 15:27:01 +0200

    Poprawne obliczanie średniej z jednego przejazdu, ale nie oblicza średniej ze wszystkich.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,45 +10,47 @@ public class Main {
 
         RoadData roadData = new RoadData(1,0,0);
 
+
+        int totalFuel = 0;
+        int totalDistance = 0;
+
+
         while (roadData.getNumber() > 0) {
 
-
-            int totalFuel = 0;
-            int totalDistance = 0;
-            double totalAverage = 0;
-            double average = 0;
-
             System.out.println("Aby zakończyć wpisz 0");
-            System.out.println("Wpisz numer trasy: ");
+            System.out.print("Wpisz numer trasy: ");
+
+
             int theNumber = input.nextInt();
-            if (theNumber != -1) {
+            roadData.setNumber(theNumber);
+
+            if (theNumber != 0) {
 
 
-                roadData.setNumber(theNumber);
-
-                System.out.println("Wpisz liczbę przejechanych kilometrów ");
+                System.out.print("Wpisz liczbę przejechanych kilometrów: ");
                 int theDistance = input.nextInt();
                 roadData.setDistance(theDistance);
 
-                System.out.println("Wpisz spaleone paliwo: ");
+                System.out.print("Wpisz spaleone paliwo: ");
                 int theFuel = input.nextInt();
                 roadData.setFuel(theFuel);
 
-                average = roadData.getFuel() * 100 / roadData.getDistance();
+                double average = (double) roadData.getFuel() * 100 / roadData.getDistance();
                 totalDistance = totalDistance + roadData.getDistance();
                 totalFuel = totalFuel + roadData.getFuel();
 
 
-                System.out.printf("Srednie spalanie z tej trasy wynosi: %s%n", average);
+                System.out.printf("Srednie spalanie z tej trasy wynosi: %.2f l/100km %n",average);
 
             }
 
-            /*else {
+            else {
+                double totalAverage = (double) totalFuel * 100 / (double) totalDistance;
+                System.out.printf("Srednia ze wszystkich przejazdów: %.2f l/100 km %n",totalAverage);
 
-            }*/
+            }
 
-            totalAverage = totalFuel * 100 / totalDistance;
-            System.out.printf("Srednia ze wszystkich przejazdów: %s%n", totalAverage);
+
 
         }
 
